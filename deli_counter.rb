bda6cf509cc3a7785bfcbf2d108fe6a40eb8459a
Stephen Courtier <steves0089@gmail.com>
2018-09-10 13:25:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 # Write your code here.
 
-katz_deli = []
 
-def line(line)
+
+def line(katz_deli)
   if line.size == 0 
     puts "The line is currently empty."
   else
-    break
+    statement = ["The line is currently: "]
+    line.each_w
+    1. Logan 2. Avi 3. Spencer"
   end
 end
