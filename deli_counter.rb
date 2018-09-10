332f907494a7224b56045d9292e040453ce9e0dd
Stephen Courtier <steves0089@gmail.com>
2018-09-10 13:54:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.size == 0 
-    puts "There is nobody waiting to be served!"
-  else
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   end
