3ee53b6bc0c3db5d14d1513b73665bc411af558a
Stephen Courtier <steves0089@gmail.com>
2018-09-10 13:32:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,5 @@
 
 
 def line(line, name)
-  if line.size == 0 
-    puts "The line is currently empty."
-  else
-    statement = ["The line is currently:"]
-    line.each_with_index do |name, index|
-      statement.push(" #{index + 1}. #{name}"
-    end
-  end
-  statement
+
 end
