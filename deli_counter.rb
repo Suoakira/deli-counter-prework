ff170117e235bc3b327611d0e04d1d35b46dab55
Stephen Courtier <steves0089@gmail.com>
2018-09-10 13:54:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-  end
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
